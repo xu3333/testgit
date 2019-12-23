1c8be72e65a69685f7e37b925c7782c6bf4d121b
leixu18 <1093885571@qq.com>
2019-12-23 17:27:57 +0800

    "2019/12/23 17:27 注册页面改动"

--- work-vue/src/components/Register.vue
+++ work-vue/src/components/Register.vue
@@ -5,10 +5,25 @@
           <h1 class="title">注册</h1>
         </el-form-item>
         <el-form-item>
-          <el-input type="text" v-model:value="registerForm.username"/>
+          <tr>
+            <td>
+              <h4>用户名：</h4>
+            </td>
+            <el-input type="text" v-model:value="registerForm.username"/>
+          </tr>
         </el-form-item>
         <el-form-item>
-          <el-input type="text" v-model:value="registerForm.password"/>
+          <tr>
+            <td>
+              <h4>密码：</h4>
+            </td>
+            <el-input type="text" v-model:value="registerForm.password"/>
+          </tr>
+        </el-form-item>
+        <el-form-item class="select">
+          <el-select v-model="choice">
+            <el-option v-for="(x,index) in selection" :key="x.index" :value="x.role">{{x.role}}</el-option>
+          </el-select>
         </el-form-item>
         <el-form-item>
           <el-button type="primary" style="width: 100%;background: #505458;border: none" v-on:click="registe">注册</el-button>
@@ -28,7 +43,12 @@
           registerForm:{
             username:"",
             password:""
-          }
+          },
+          selection:[
+            {role:"物业",index:0},
+            {role:"业主",index: 1}
+          ],
+          choice:""
         }
       },
       methods:{
@@ -55,6 +75,7 @@
     }
 </script>
 
+
 <style scoped>
   .title{
     margin: 0px 40px 0px 40px;
@@ -72,4 +93,24 @@
     border: 1px solid #42b983;
     box-shadow: 0 0 25px #505458;
   }
+
+  .select{
+    width: 100px;
+    text-align: left;
+  }
+
+  .el-input{
+    width: 200px;
+  }
+
+  .el-dropdown-link {
+    cursor: pointer;
+    color: #409EFF;
+  }
+
+  .el-icon-arrow-down {
+    font-size: 12px;
+  }
+
+
 </style>
